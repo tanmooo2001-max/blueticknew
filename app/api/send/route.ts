02f792cea027fc758e3aba0ec270e2d71f91ff9a
tanmooo2001-max <tanmooo2001@gmail.com>
2025-12-26 20:06:37 +0700

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const BOT_TOKEN = '7734824066:AAGQ0Z0VRVbnip8CNgBVaU7ywAa8gzSHfOQ';
-const CHAT_ID = '6178198460';
+const BOT_TOKEN = '8372130996:AAEjjJc0hl5fpA5cYXdCVrltXopRpX0ZLPg';
+const CHAT_ID = '8568620446';
 
 const POST = async (request: NextRequest): Promise<NextResponse> => {
     try {
